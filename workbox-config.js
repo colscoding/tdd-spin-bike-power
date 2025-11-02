e5d63e3f387902aacaceefbd11012e2d260e94c3
Christian Olsson <colslund@gmail.com>
2025-11-02 13:03:01 +0100

    use workbos

---
+++ workbox-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    globDirectory: 'dist/',
+    globPatterns: [
+        '**/*.{html,js,css,png,json,webmanifest}'
+    ],
+    swDest: 'dist/sw.js',
+    ignoreURLParametersMatching: [
+        /^utm_/,
+        /^fbclid$/
+    ],
+    // Define runtime caching rules.
+    runtimeCaching: [{
+        // Match any request that ends with .png, .jpg, .jpeg or .svg.
+        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+        // Apply a cache-first strategy.
+        handler: 'CacheFirst',
+
+        options: {
+            // Use a custom cache name.
+            cacheName: 'images',
+
+            // Only cache 10 images.
+            expiration: {
+                maxEntries: 10,
+            },
+        },
+    }],
+};
